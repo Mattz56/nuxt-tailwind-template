14cc2e864d4c0e5a5fba821429fe6c880c1677c9
Mattz56 <Matthieu.audo.56@gmail.com>
2025-09-21 13:43:45 -0400

    Add files

---
+++ app/components/Test.vue
@@ -0,0 +1,9 @@
+<template>
+  <div
+    class="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white"
+  >
+    <h3 class="text-8xl font-bold underline">
+      Welcome to Nuxt-Tailwind template
+    </h3>
+  </div>
+</template>
