4c86e0495cde4802139968f8648544083517646c
Mattz56 <Matthieu.audo.56@gmail.com>
2025-09-21 13:53:52 -0400

    First Commit

--- app/components/Test.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div
-    class="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white"
-  >
-    <h3 class="text-8xl font-bold underline">
-      Welcome to Nuxt-Tailwind template
-    </h3>
-  </div>
-</template>
